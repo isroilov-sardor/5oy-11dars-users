efe68482a34373004c9af4d1a672c7461d27c7c7
isroilov-sardor <notfalse01@gmail.com>
2024-11-10 22:17:48 +0500

    done

---
+++ js/index.js
@@ -0,0 +1,71 @@
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".loader").style.display = "none";
+    }, 0);
+});
+
+function createCard(value) {
+    return `
+    <div class="block">
+                    <div class="block-same">
+                        <div class="block-name">${value.navElement}</div>
+                        <div class="block-id">id: ${value.id}</div>
+                    </div>
+                    <div class="block-username">username: ${value.username}</div>
+                    <div class="block-email">email: ${value.email}</div>
+                    <div class="block-phone">phone: ${value.phone}</div>
+                    <div class="block-website">site: ${value.website}</div>
+                </div>
+    `;
+}
+const wrapper = document.querySelector(".end-wrapers");
+const button = document.querySelector("#end-btn");
+
+button &&
+    button.addEventListener("click", () => {
+        fetch("https://jsonplaceholder.typicode.com/users", {
+            method: "GET",
+        })
+            .then((response) => {
+                if (response.status == 200) {
+                    return response.json();
+                }
+                if (response.status == 404) {
+                    throw new Error("API not working or something happen");
+                }
+            })
+            .then((data) => {
+                data.forEach((element) => {
+                    const data = createCard(element);
+                    wrapper.innerHTML += data;
+                });
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    });
+
+const hoveredElement = document.querySelector("#head-link");
+const navElement = document.querySelector(".efect");
+
+hoveredElement.addEventListener("mouseenter", function (event) {
+    navElement.style.display = "block";
+});
+
+hoveredElement.addEventListener("mouseleave", () => {
+    navElement.style.display = "none";
+});
+
+const element = document.querySelector(".mid-desc");
+const hovElement = document.querySelector(".mid-thing");
+
+element &&
+    element.addEventListener("mouseenter", function (event) {
+        event.preventDefault();
+        hovElement.style.display = "block";
+    });
+element &&
+    element.addEventListener("mouseleave", function (event) {
+        event.preventDefault();
+        hovElement.style.display = "none";
+    });
